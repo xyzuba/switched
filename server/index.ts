94487fd343138d498a7142d36a86535a37db1a14
xyzuba <egoriy22501@gmail.com>
2021-11-20 15:04:33 +0200

    init

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { createConnection } from "typeorm";
+import express from "express";
+import session from "express-session";
+
+const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    database: "switched",
+    username: "postgres",
+    password: "postgres",
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, "/migrtions/*")],
+    entities: [],
+  });
+
+  conn.runMigrations();
+
+  const app = express();
+
+  app.listen;
+};
+
+main();
